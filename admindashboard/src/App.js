7c517694d115237b4a0470a08d0081693781d344
nang <nangsandaraung@u.nus.edu>
2021-02-15 02:09:06 +0800

    Layout Component
    Signed-off-by: nang <nangsandaraung@u.nus.edu>

--- admindashboard/src/App.js
+++ admindashboard/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import Test from './components/Test'
+import Nav from './Layout/Nav'
 
 class App extends Component{
   render(){
     return(
       <div className="App">
+        <Nav />
         <h1>Welcome Class Component</h1>
         <Test />
       </div>
